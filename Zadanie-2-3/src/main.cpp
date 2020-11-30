41befa229f2dbdcb475de09ceca51fe7986b1ad8
Emil Woźniak <emil.wozniak01@student.wat.edu.pl>
2020-11-30 09:51:14 +0100

    Rozpoczęto zadanie 2.3 - IRQ

---
+++ Zadanie-2-3/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+int main() {
+  init();
+  while (1) { /* code */ }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
